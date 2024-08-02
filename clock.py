1db10c69b93280b363fda7826f4fa14deafc1350
Manasvin5 <manasvinshrimali@gmail.com>
2024-08-02 16:31:51 +0530

    hi

--- clock.py
+++ clock.py
@@ -10,34 +10,23 @@ class DigitalClock(QWidget):
         
     def initUI(self):
         self.setWindowTitle('Digital Clock')
-        self.setGeometry(100, 100, 500, 250)  # Adjusted size for AM/PM
+        self.setGeometry(100, 100, 500, 250)  # AM PM ke liye size
         
-        # Timer to update the time every second
+        # every sec update
         timer = QTimer(self)
         timer.timeout.connect(self.update)
         timer.start(1000)
-        
         self.show()
-    
     def paintEvent(self, event):
         current_time = QTime.currentTime()
         display_text = current_time.toString('hh:mm:ss AP')  # 12-hour format with AM/PM
-        
-        # Customize font
         font = QFont('Helvetica', 72, QFont.Bold)
-        
         painter = QPainter(self)
         painter.setRenderHint(QPainter.Antialiasing)
         painter.setFont(font)
-        
-        # Customize background
         painter.setBrush(QBrush(QColor(30, 30, 30)))
         painter.drawRect(self.rect())
-        
-        # Customize text color
         painter.setPen(QColor(255, 255, 255))
-        
-        # Draw time
         rect = self.rect()
         painter.drawText(rect, Qt.AlignCenter, display_text)
 
